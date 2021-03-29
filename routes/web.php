be8b076fdfabeb0cb69644e36b338c55b5b15932
moemi <meiyin6888@gmail.com>
2021-03-29 06:37:04 +0000

    webphp

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create','Admin\NewsController@add');
+});
+
+//課題3
+Route::get('XXX','AAAController@bbb');
+
+//課題4
+Route::group(['prefix' => 'admin'],function(){
+    Route::get('profile/create','Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+});
