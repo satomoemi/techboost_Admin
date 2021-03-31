b80dc680af3f17d568409dcb83350b2e945bbb2c
moemi <meiyin6888@gmail.com>
2021-03-31 10:09:01 +0000

    blade_laravelmix

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create','Admin\NewsController@add');
+    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
 });
 
 //課題3
@@ -27,3 +27,7 @@ Route::group(['prefix' => 'admin'],function(){
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
