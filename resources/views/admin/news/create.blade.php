a1e7442fcaa4016a6cc4a86aab1fab4095a1d383
moemi <meiyin6888@gmail.com>
2021-04-01 11:01:27 +0000

    login.create.edit.profile.blade_web.php

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -1,7 +1,8 @@
+{{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
-
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
 @section('title', 'ニュースの新規作成')
-
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
