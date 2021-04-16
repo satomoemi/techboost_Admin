15c1ba6fa9d38cdfe1e99b364a1a53dfe2fd8fe8
moemi <meiyin6888@gmail.com>
2021-04-16 13:18:32 +0000

    Nindex

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -11,12 +11,16 @@ class ProfileController extends Controller
 {
     public function index(Request $request)
     {
-        $posts = Profile::all();
+        $posts = Profile::all()->sortByDesc('updated_at');
 
-        
+        if (count($posts) > 0) {
+            $headline = $posts->shift();
+        } else {
+            $headline = null;
+        }
 
         // profile/index.blade.php ファイルを渡している
         // また View テンプレートに headline、 posts、という変数を渡している
-        return view('profile.index', ['posts' => $posts]);
+        return view('profile.index', ['headline' => $headline, 'posts' => $posts]);
     }
 }
